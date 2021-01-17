084ef61cf03d9318c5fc1f3e4c0fb8c971096e4b
Gabriel Stamato <stamato7@gmail.com>
2021-01-16 23:06:03 -0300

    add progress

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
 import App from "next/app";
 import Head from "next/head";
+import Router from 'next/router'
 import { ThemeProvider } from "styled-components";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import NProgress from "nprogress";
 
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
@@ -14,6 +16,13 @@ import { theme } from "../styles/theme";
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+
+Router.events.on("routeChangeStart", (url) => {
+  console.log(`Loading: ${url}`);
+  NProgress.start();
+});
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
@@ -34,12 +43,10 @@ class MyApp extends App {
             name="keywords"
             content="APVA - Associação Protetora da Vida Animal - Ajudando os nossos amigos de 4 patas de Paulínia e região"
           />
-          <meta
-            name="og:image"
-            content="/assets/Logo.png"
-          />
+          <meta name="og:image" content="/assets/Logo.png" />
           <meta charSet="UTF-8" />
           <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="stylesheet" type="text/css" href="/nprogress.css" />
         </Head>
         <GlobalStyle />
         <ToastContainer />
